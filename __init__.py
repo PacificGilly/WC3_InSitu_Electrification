5e54a63e2c4c8b8c07e631379113559ddee20031
James Gilmore <james.gilmore@pgr.reading.ac.uk>
2018-11-17 19:40:26 +0000

    Updated __init__ to version 2.1

--- __init__.py
+++ __init__.py
@@ -1,7 +1,8 @@
 from __future__ import absolute_import, division, print_function
 import warnings
 
-__version__ = 2.0
+__version__ = 2.1
+__doc__ = "Gilly Utilities is a module which adds extra support for common data analysis problems. The functions and classes in this module are mainly extensions of the numpy package with some extensions for scipy and pandas."
 
 with warnings.catch_warnings():
 	warnings.simplefilter("ignore")
